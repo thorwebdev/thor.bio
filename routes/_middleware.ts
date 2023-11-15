b0a30b689288f91edabb87a3e56050dba9cd6855
thorwebdev <thor@supabase.io>
2023-11-16 01:10:49 +0900

    fix: qrswag redirect.

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -102,7 +102,7 @@ export const handler = [
         return Response.redirect("https://cal.com/thorwebdev/15min", 307);
       case "/30":
         return Response.redirect("https://cal.com/thorwebdev/30min", 307);
-      case "/qr":
+      case "/qrswag":
         return Response.redirect("https://supaswagstore.deno.dev/", 307);
     }
     return ctx.next();
